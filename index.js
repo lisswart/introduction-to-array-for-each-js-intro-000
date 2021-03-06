bf8ac9f53aa50cebf19449d5fc8efbb85932bd0b
Lisa Swart <arnoudlisa@gmail.com>
2021-03-06 13:38:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+
+const array1 = ["apple", "banana", "cherry"];
+doToElementsInArray(array1);
+
+const array2 = ["antelope", "bear", "cat"];
+doToElementsInArray(array2, changeCompletely);
